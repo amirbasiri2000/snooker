a856e130017aba3bda6dc14278120a2d7a00292f
amirbasiri2000 <basiriamir2000@gmail.com>
2022-11-12 23:14:54 +0330

    login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,7 @@
+import LoginPage from "../../components/login/Loing";
+
+export default function Login() {
+  return <div className="w-full flex justify-center items-center h-full">
+    <LoginPage  />
+  </div>;
+}
