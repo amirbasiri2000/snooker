7aac955fbdcc908a607b2abe64cd501ea43b6802
amirbasiri2000 <basiriamir2000@gmail.com>
2022-11-13 15:41:11 +0330

    register

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,11 @@
+import SignInPage from "../components/sign-in/SingIn";
+
+
+export default function SignIn () {
+
+    return (
+        <div className="w-full flex justify-center items-center h-full">
+            <SignInPage />
+        </div>
+    )
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,11 @@
+import SignUpPage from "../components/sign-up/SingUp";
+
+
+export default function SingUp() {
+
+    return (
+        <div className="w-full flex justify-center items-center h-full">
+            <SignUpPage />
+        </div>
+    )
+}
